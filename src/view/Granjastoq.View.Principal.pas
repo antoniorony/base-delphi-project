506220c50f867f8b2f648c2eebbdd3c02b9ab0e7
antoniorony <antoniorony2013@gmail.com>
2024-02-17 14:41:30 -0300

    Add: menu lateral.

--- src/view/Granjastoq.View.Principal.pas
+++ src/view/Granjastoq.View.Principal.pas
@@ -15,22 +15,42 @@ uses
   Vcl.ExtCtrls,
   Vcl.WinXCtrls,
   Vcl.Menus,
-  Rtti;
+  Rtti, VCLTee.TeCanvas, Vcl.StdCtrls, System.ImageList, Vcl.ImgList,
+  Vcl.Imaging.pngimage, Vcl.Buttons;
 
 type
   TViewPrincipal = class(TForm)
     mmMenuPrincipal: TMainMenu;
     Menu1: TMenuItem;
     Usuarios1: TMenuItem;
+    pnlPrincipal: TPanel;
+    pnlLateral: TPanel;
+    il1: TImageList;
+    pnl1: TPanel;
+    img1: TImage;
+    lblName: TLabel;
+    btnHome: TSpeedButton;
+    pnlAdministracao: TPanel;
+    imgAdministracao: TImage;
+    lblAdministracao: TLabel;
+    btnAdministracao: TSpeedButton;
+    pnlCentral: TPanel;
+    pnlUsuario: TPanel;
+    imgUsuario: TImage;
+    lblUsuario: TLabel;
+    btnUsuario: TSpeedButton;
     procedure FormActivate(Sender: TObject);
     procedure Usuarios1Click(Sender: TObject);
+    procedure btnHomeClick(Sender: TObject);
+    procedure btnAdministracaoClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
-  //Enums
+  // Enums
   TListaDeMenus = (Usuarios, Help);
 
 var
@@ -46,11 +66,43 @@ uses
 
 {$R *.dfm}
 
+procedure TViewPrincipal.btnAdministracaoClick(Sender: TObject);
+begin
+  pnlCentral.Visible := True;
+end;
+
+procedure TViewPrincipal.btnHomeClick(Sender: TObject);
+begin
+  var
+    I: Integer;
+
+  if TSpeedButton(Sender).Width > 159 then
+    begin
+      I := 50;
+      btnHome.Width := I;
+      pnl1.Width := I;
+      pnlLateral.Width := I+2;
+
+      btnAdministracao.Width := I;
+      pnlAdministracao.Width := I;
+    end
+  else
+    begin
+      I := 168;
+      btnHome.Width := I;
+      pnl1.Width := I;
+      pnlLateral.Width := I+2;
+
+      btnAdministracao.Width := I;
+      pnlAdministracao.Width := I;
+    end;
+end;
+
 procedure TViewPrincipal.FormActivate(Sender: TObject);
 var
   LoginView: TLoginView;
 begin
-  LoginView:= TLoginView.create(nil);
+  LoginView := TLoginView.create(nil);
   try
     LoginView.ShowModal();
   finally
@@ -58,11 +110,17 @@ begin
   end;
 end;
 
+procedure TViewPrincipal.FormShow(Sender: TObject);
+begin
+  Self.Color := $00F1F2F3;
+  pnlLateral.Color := $00F1F2F3;
+end;
+
 procedure TViewPrincipal.Usuarios1Click(Sender: TObject);
 var
   lUsuarios: TGestaoLoginView;
 begin
-  lUsuarios := TGestaoLoginView.Create(nil);
+  lUsuarios := TGestaoLoginView.create(nil);
   try
     lUsuarios.ShowModal();
   finally
